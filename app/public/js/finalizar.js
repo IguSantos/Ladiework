3d1edd52abda22c1602b015bbfd2bd29f6e94dac
IguSantos <105032129+IguSantos@users.noreply.github.com>
2024-04-03 15:52:37 -0300

    Delete app directory

--- app/public/js/finalizar.js
+++
@@ -1,8 +0,0 @@
-function toggleForm(formId) {
-    var form = document.getElementById(formId);
-    if (form.style.display === "none") {
-        form.style.display = "block";
-    } else {
-        form.style.display = "none";
-    }
-}
