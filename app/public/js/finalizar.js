83633ef13b20bb1898f12a94c50ad090c8cdaddb
IguSantos <105032129+IguSantos@users.noreply.github.com>
2024-04-26 20:58:44 -0300

    Adicionando opções para a compra

--- app/public/js/finalizar.js
+++ app/public/js/finalizar.js
@@ -1,37 +1,45 @@
 function toggleForm(formId) {
-    var form = document.getElementById(formId);
-    if (form.style.display === "none") {
-        form.style.display = "block";
-    } else {
-        form.style.display = "none";
-    }
+  var form = document.getElementById(formId);
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var modulos = document.querySelectorAll('.card');
-  
-    modulos.forEach(function(modulo) {
-      modulo.addEventListener('click', function() {
-        var aulas = this.querySelector('.aulas');
-        toggleDisplay(aulas);
-  
-        // Esconder as aulas de outros módulos
-        var outrosModulos = document.querySelectorAll('.modulo');
-        outrosModulos.forEach(function(outroModulo) {
-          if (outroModulo !== modulo) {
-            var outrasAulas = outroModulo.querySelector('.aulas');
+document.addEventListener('DOMContentLoaded', function () {
+  var modulos = document.querySelectorAll('.card');
+
+  modulos.forEach(function (modulo) {
+    modulo.addEventListener('click', function (event) {
+      if (!event.target.matches('input[type="text"]') && !event.target.matches('input[type="number"]')) {
+        var cardInfo = this.querySelector('.card-info');
+        toggleDisplay(cardInfo);
+
+        var outrosModulos = document.querySelectorAll('.card');
+        outrosModulos.forEach(function (anotherModule) {
+          if (anotherModule !== modulo) {
+            var outrasAulas = anotherModule.querySelector('.card-info');
             outrasAulas.style.display = 'none';
           }
         });
-      });
+      }
     });
   });
-  
-  function toggleDisplay(element) {
-    if (element.style.display === 'none') {
-      element.style.display = 'block';
-    } else {
-      element.style.display = 'none';
-    }
+});
+
+function toggleDisplay(element) {
+  element.style.display = (element.style.display === "none") ? "block" : "none";
+}
+
+
+function toggleDisplay(element) {
+  if (element.style.display === 'none') {
+    element.style.display = 'block';
+  } else {
+    element.style.display = 'none';
   }
-  
+}
+
+
+
