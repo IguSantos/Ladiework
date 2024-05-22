ee990f3bc62fc8bb47bf61002dfd01acd373f8aa
IguSantos <105032129+IguSantos@users.noreply.github.com>
2024-05-22 20:45:10 -0300

    Pagamento pix lindo maravilhoso, eu que fiz

--- app/public/js/pagamentopix.js
+++ app/public/js/pagamentopix.js
@@ -1,20 +1,52 @@
-const button = document.querySelector(".botao");
-const rectangle1 = document.querySelector(".retangulo1");
-const rectangle2 = document.querySelector(".retangulo2");
-
-button.addEventListener("click", function () {
-    rectangle1.classList.add("hidden");
-    rectangle2.classList.remove("hidden");
-});
-
-rectangle2.addEventListener("click", function () {
-    rectangle1.classList.remove("hidden");
-    rectangle2.classList.add("hidden");
-});
-
-button.addEventListener("click", function () {
-    rectangle1.style.display = "none";
-    rectangle2.style.display = "block";
-});
-
-
+document.addEventListener("DOMContentLoaded", function() {
+    const useCodeButton = document.getElementById("use-code");
+    const useIdButton = document.getElementById("use-id");
+    const pixKeyArticle = document.getElementById("pix-key");
+    const qrCodeArticle = document.getElementById("qr-code");
+  
+    // Função para verificar o tamanho da janela e reverter as alterações
+    function resetDisplay() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth > 700) {
+        pixKeyArticle.style.display = "block";
+        qrCodeArticle.style.display = "flex";
+      } else {
+        pixKeyArticle.style.display = "block";
+        qrCodeArticle.style.display = "none";
+      }
+    }
+  
+    // Verificar e redefinir quando a página é carregada
+    resetDisplay();
+  
+    // Verificar e redefinir quando a janela é redimensionada
+    window.addEventListener("resize", resetDisplay);
+  
+    useCodeButton.addEventListener("click", function() {
+      pixKeyArticle.style.display = "none";
+      qrCodeArticle.style.display = "flex";
+    });
+  
+    useIdButton.addEventListener("click", function() {
+      pixKeyArticle.style.display = "block";
+      qrCodeArticle.style.display = "none";
+    });
+  });
+  
+document.addEventListener("DOMContentLoaded", function() {
+    const controlvButton = document.getElementById("controlv");
+    const uniqueIdentificatorInput = document.getElementById("unique-identificator");
+  
+    controlvButton.addEventListener("click", function() {
+      // Tenta copiar o conteúdo da área de transferência do sistema
+      navigator.clipboard.readText()
+        .then(text => {
+          // Define o valor do input com o texto copiado
+          uniqueIdentificatorInput.value = text;
+        })
+        .catch(err => {
+          console.error('Falha ao ler o texto da área de transferência: ', err);
+        });
+    });
+  });
+  
