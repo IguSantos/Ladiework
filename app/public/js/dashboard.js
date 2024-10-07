c6bccf1b8722f8b6fd2e908431abac4216de7106
Your Name <you@example.com>
2024-10-07 15:50:58 -0300

    Formulário dashboard atualizado

--- app/public/js/dashboard.js
+++ app/public/js/dashboard.js
@@ -18,3 +18,21 @@ document.addEventListener("DOMContentLoaded", function() {
     homeSection.style.display = "grid"; // Ou a configuração original que você tem
   });
 });
+
+const thumbnailInput = document.getElementById('thumbnail');
+const thumbnailPreview = document.getElementById('thumbnail-preview');
+
+thumbnailInput.addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      thumbnailPreview.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+document.getElementById("price").addEventListener("input", function (e) {
+  this.value = this.value.replace(/[^0-9]/g, ''); // Remove qualquer caractere não numérico
+});
